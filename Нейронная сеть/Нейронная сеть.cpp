c3f0e36c0ba48946508d1885234c687e756b975c
Shi Warai <40498648+ShiWarai@users.noreply.github.com>
2020-10-22 13:31:38 +0300

    Добавлено чтение папки тренировочных данных

--- Нейронная сеть/Нейронная сеть.cpp
+++ Нейронная сеть/Нейронная сеть.cpp
@@ -1,9 +1,47 @@
 ﻿#include <iostream>
+#include <fstream>
+#include <string>
+#include <Windows.h>
+#include <vector>
 
 using namespace std;
 
 int main()
 {
-    cout << "Hello World!\n";
 
+	setlocale(LC_ALL, "");
+	
+	const wstring PATH = L"C:/Digits/";
+	vector<vector<string>> trainingFiles;
+
+	WIN32_FIND_DATA FindFileData;
+	HANDLE handleFiles = FindFirstFileW((PATH + L"*").c_str(), &FindFileData);
+
+	if (handleFiles != INVALID_HANDLE_VALUE)
+	{
+		while (true) {			
+			if (FindNextFileW(handleFiles, &FindFileData) != NULL)
+			{
+				wstring ws(FindFileData.cFileName);
+				string fileName(ws.begin(), ws.end());
+				string digit = fileName.substr(0, fileName.find("."));
+				
+				trainingFiles.push_back(vector<string> {fileName, digit});
+			}
+			else
+			{
+				break;
+			}
+		}
+	}
+
+	trainingFiles.erase(trainingFiles.begin()); // Удаляем скрытый файл ".."
+	FindClose(handleFiles);
+
+	// Вывод названий и цифр
+	for(int i = 0; i < trainingFiles.size(); i++)
+		cout << "File name:" << trainingFiles[i][0] << "\nDigit:" << trainingFiles[i][1] << endl;
+	
+	
+	return 0;
 }
