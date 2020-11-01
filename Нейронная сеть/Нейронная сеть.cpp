df57a93755ebbd39226451da2a3e2903f7958f98
Shi Warai <40498648+ShiWarai@users.noreply.github.com>
2020-11-01 06:41:15 +0300

    Добавлен второй слой, flatten

--- Нейронная сеть/Нейронная сеть.cpp
+++ Нейронная сеть/Нейронная сеть.cpp
@@ -53,6 +53,36 @@ vector<vector<double>> dot(vector<vector<double>> a, vector<vector<double>> b) {
 	return c;
 }
 
+// a + b = c (поэлементное сложение)
+vector<vector<double>> sum(vector<vector<double>> a, vector<vector<double>> b) {
+
+	if ((a.size() != b.size()) && (a[0].size() != b[0].size())) {
+		cout << "Размеры массивов не совпадают" << endl;
+		exit(-1);
+	}
+
+	vector<vector<double>> c = createFilledVector(a.size(), a[0].size());
+
+	for (int y1 = 0; y1 < a.size(); y1++) {
+		for (int x1 = 0; x1 < a[0].size(); x1++) {
+			c[y1][x1] = a[y1][x1] + b[y1][x1];
+		}
+	}
+
+	return c;
+}
+
+double elementsSum(vector<vector<double>> a) {
+	double sum = 0;
+	for (int y1 = 0; y1 < a.size(); y1++) {
+		for (int x1 = 0; x1 < a[0].size(); x1++) {
+			sum += a[y1][x1];
+		}
+	}
+
+	return sum;
+}
+
 // Поэлементный максимум
 vector<vector<double>> reluFunction(vector<vector<double>> a) {
 
@@ -167,8 +197,25 @@ vector<vector<double>> matrixExpansion(vector<vector<double>> matrix, unsigned p
 
 }
 
+// Вырезка из matrix прямоуголника AxB с координат y0, x0 (Добавить защиту!!!)
+vector<vector<double>> matrixSlicer(vector<vector<double>> matrix, unsigned y0, unsigned x0, unsigned a, unsigned b) { 
+	auto processed_matrix = createFilledVector(a, b);
+
+	for (int y = 0; y < a; y++) {
+		for (int x = 0; x < b; x++) {
+			processed_matrix[y][x] = matrix[y0+y][x0+x];
+		}
+	}
+
+	return processed_matrix;
+}
+
 // Главная функция прямого распространения
 vector<vector<double>> directDistributionFunc(vector<vector<vector<double>>> pic, vector<vector<vector<double>>> core) {
+
+	if (core.size() != pic.size())
+		exit(0);
+
 	unsigned y_size = pic[0].size();
 	unsigned x_size = pic[0][0].size();
 
@@ -176,24 +223,27 @@ vector<vector<double>> directDistributionFunc(vector<vector<vector<double>>> pic
 	auto processed_pic = createFilledVector(y_size, x_size);
 
 	int dimensions = pic.size();
+	int panding_ = (int)ceil((double)(core[0].size() - 1) / 2); // Определяем начало гл. массива
 
-	for (int y = 0; y < y_size; y++) {
-		for (int x = 0; x < x_size; x++) {
-			double sum = 0;
+	vector<vector<double>> flat;
+	for (int dim = 0; dim < dimensions; dim++) {
 
-			for (int dim = 0; dim < dimensions; dim++) {
+		// Работа с плоскостью
+		flat = matrixExpansion(pic[dim], core[0].size() - 1); // Получаем расширенную матрицу плоскости
 
-				// Работа с плоскостью
-				vector<vector<double>> flat = matrixExpansion(pic[dim], core.size() - 1);
+		for (int y = 0; y < y_size; y++) {
+			for (int x = 0; x < x_size; x++){
 
+				auto slice = matrixSlicer(flat, panding_ + y - (core[dim].size() - 1), panding_ + x - (core[dim][0].size() - 1), core[dim].size(), core[dim][0].size()); // Получаем кусок матрицы
+				slice = dot(slice, core[dim]); // Находим произведение
 
+				processed_pic[y][x] += elementsSum(slice); // Складываем слои
 			}
-
-			processed_pic[y][x] = sum;
 		}
 	}
 
-	return processed_pic;
+	// Выводим с использованием relu()
+	return reluFunction(processed_pic);
 }
 
 // Свёртка к максимальному
@@ -217,13 +267,14 @@ vector<vector<double>> max_pooling(vector<vector<double>> a) {
 		finalBuffer.push_back(buffer);
 	}
 
+	/*
 	for (int y = 0; y < currentSize / 2; y++) {
 		for (int x = 0; x < currentSize / 2; x++) {
 			a[y][x] = finalBuffer[y][x];
 			cout << setw(6) << setprecision(4) << a[y][x] << " ";
 		}
 		cout << endl;
-	}
+	} */
 
 	return finalBuffer;
 }
@@ -245,6 +296,16 @@ vector<double> softmax(vector<vector<double>> a) {
 	return c;
 }
 
+vector<double> flatten(vector<vector<double>> a) {
+	vector<double> b;
+	for (int y = 0; y < a.size(); y++) {
+		for (int x = 0; x < a.size(); x++)
+			b.push_back(a[y][x]);
+	}
+
+	return b;
+}
+
 // Первое заполнение ядер
 vector<vector<vector<double>>> generationCore(unsigned CORE_SIZE, unsigned DEPTH) {
 	vector<vector<vector<double>>> core;
@@ -257,7 +318,7 @@ vector<vector<vector<double>>> generationCore(unsigned CORE_SIZE, unsigned DEPTH
 
 		for (int y1 = 0; y1 < CORE_SIZE; y1++) {
 			for (int x1 = 0; x1 < CORE_SIZE; x1++) {
-				core[i][y1][x1] = (double)(rand() % 10);
+				core[i][y1][x1] = 1 - ((double)rand() /(RAND_MAX+1)) * 2;
 			}
 		}
 	}
@@ -268,6 +329,14 @@ vector<vector<vector<double>>> generationCore(unsigned CORE_SIZE, unsigned DEPTH
 // Программа
 int main()
 {
+	/*
+	vector<vector<double>> a = { {1,4,2},{32,2,0},{5,4,7} };
+	auto b = flatten(a);
+
+	for (int y1 = 0; y1 < b.size(); y1++) {
+		cout << setw(3) << b[y1];
+	}
+	*/
 
 	const unsigned int PICTURE_SIZE = 16;
 	srand(time(NULL));
@@ -338,7 +407,7 @@ int main()
 
 	// Итерации обучения (прямой и обратный ход)
 	for (int epoch = 1; epoch <= epochs; epoch++) {
-		for (int fileNum = 0; fileNum < trainingFiles.size(); fileNum++) {
+		for (int fileNum = 0; fileNum < 1; fileNum++) { // trainingFiles.size()
 
 			BMP_BW image(trainingFiles[fileNum][1], (string)(PATH_S + trainingFiles[fileNum][0]), false);
 			cout << "(" << epoch << ", " << trainingFiles[fileNum][0] << ") :" << endl;
@@ -347,6 +416,7 @@ int main()
 			vector<vector<vector<vector<double>>>> cores_set;
 			int output_dim = 32;
 
+			// Генерация или чтение набора ядер
 			if (epoch == 1 && fileNum == 0) {
 
 				for (int i = 0; i < output_dim; i++) {
@@ -354,27 +424,75 @@ int main()
 					cores_set[i] = generationCore(2, 1);
 				}
 
-				cores[0] = cores_set;
+				cores.push_back(cores_set);
 			}
 			else {
 				cores_set = cores[0];
 			}
 
-			vector<vector<vector<double>>> processed_images;
+			vector<vector<vector<double>>> layers1;
 
 			vector<vector<double>> new_matrix;
 			for (int i = 0; i < output_dim; i++) {
 				auto core = cores_set[i];
 				new_matrix = directDistributionFunc(vector<vector<vector<double>>> {image.getImage()}, core);
 
-				processed_images.push_back(new_matrix);
+				for (int y = 0; y < new_matrix.size(); y++) {
+					for (int x= 0; x < new_matrix[0].size(); x++) {
+						cout << setw(5) << setprecision(3)<< new_matrix[y][x];
+					}
+					cout << endl;
+				}
+				cout << endl;
+
+				// Max_pooling 2x2
+				new_matrix = max_pooling(new_matrix);
+
+				// Добавляем уже сжатый слой
+				layers1.push_back(new_matrix); 
 			}
 
 
+			cout << "2 LAYER" << endl;
 
+			// Слой 2
+			output_dim = 16;
+			cores_set.clear();
 
+			// Генерация или чтение набора ядер
+			if (epoch == 1 && fileNum == 0) {
 
+				for (int i = 0; i < output_dim; i++) {
+					cores_set.push_back(vector<vector<vector<double>>> {});
+					cores_set[i] = generationCore(2, 32);
+				}
 
+				cores.push_back(cores_set);
+			}
+			else {
+				cores_set = cores[1];
+			}
+
+			vector<vector<vector<double>>> layers2;
+
+			new_matrix.clear();
+			for (int i = 0; i < output_dim; i++) {
+				auto core = cores_set[i];
+				new_matrix = directDistributionFunc(layers1, core);
+
+				for (int y = 0; y < new_matrix.size(); y++) {
+					for (int x = 0; x < new_matrix[0].size(); x++) {
+						cout << setw(5) << setprecision(3)<< new_matrix[y][x];
+					}
+					cout << endl;
+				}
+				cout << endl;
+
+				// Max_pooling 2x2
+				new_matrix = max_pooling(new_matrix);
+
+				layers2.push_back(new_matrix);
+			}
 
 
 			delta.clear();
